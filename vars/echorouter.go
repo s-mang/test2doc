6a7a04948594ef423a7c7f8f63e10f3c3c2fb698
Piotr Mońko <piotr.monko@acaisoft.com>
2023-01-10 12:09:10 +0100

    Update Echo router extractor

--- vars/echorouter.go
+++ vars/echorouter.go
@@ -4,19 +4,35 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/s-mang/test2doc/doc/parse"
 	"net/http"
+	"sync"
 )
 
 // MakeEchoRouterExtractor is a URLVarExtractor for Echo server
 func MakeEchoRouterExtractor(e *echo.Echo) parse.URLVarExtractor {
 	router := e.Router()
+	mu := sync.Mutex{}
+
 	return func(req *http.Request) map[string]string {
-		out := make(map[string]string)
+		mu.Lock()
+		defer mu.Unlock()
+		if req == nil {
+			panic("missing request")
+		}
+
 		c := e.AcquireContext()
+		if c == nil {
+			panic("cannot AcquireContext")
+		}
+
 		router.Find(req.Method, req.URL.Path, c)
+		if c.Request() == nil {
+			c.SetRequest(req)
+		}
 		params := c.QueryParams()
 		c.Reset(nil, nil)
 		e.ReleaseContext(c)
 
+		out := make(map[string]string)
 		for k, v := range params {
 			out[k] = v[0]
 		}
